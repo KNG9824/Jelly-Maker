927640df0ae9921d90e03ffd9dcbd7f3cf6e80fe
KNG9824 <aktlwjd98@gmail.com>
2024-09-10 15:03:11 +0900

    Day3 PM

--- Jelly Maker/Assets/Scripts/Game Manager.cs
+++ Jelly Maker/Assets/Scripts/Game Manager.cs
@@ -5,4 +5,11 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Vector3[] PointList;
+
+    public RuntimeAnimatorController[] LevelAC;
+
+    public void ChangeAC(Animator anim, int level) { 
+    anim.runtimeAnimatorController= LevelAC[level-1];
+    
+    }
 }
